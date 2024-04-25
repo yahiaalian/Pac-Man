0f947648db2da4992317f7016bfd7301172afdcf
yahiaalian <yahia.ayman03@gmail.com>
2024-04-25 22:22:17 +0200

    project files

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 8.0f;
+    public float speedmultiplier = 1.0f;
+    public Vector2 initialDirection;
+    public LayerMask obstaclelayer;
+    public Rigidbody2D rigidbody1 {  get; private set; }
+    public Vector2 direction {  get; private set; }
+    public Vector2 nextdirection { get; private set; }
+    public Vector3 statingpostion { get; private set; }
+
+
+    public void Awake()
     {
-        
+        this.rigidbody1 = GetComponent<Rigidbody2D>();
+        this.statingpostion=this.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+
+    }
+
+    public void ResetState()
+    {
+        this.speedmultiplier = 1.0f;
+        this.direction = this.initialDirection;
+        this.nextdirection = Vector2.zero;
+        this.transform.position = this.statingpostion;
+        this.rigidbody1.isKinematic = false;
+        this.enabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+
     }
 }
