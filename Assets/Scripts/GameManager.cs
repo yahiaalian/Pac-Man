2db6ed309bc631a6b8d12a76a6e01bab702f6a18
yahiaalian <yahia.ayman03@gmail.com>
2024-04-25 19:58:15 +0200

    project files

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Ghosts[] ghosts;
+
+    public PacMan pacman;
+
+    public Transform pellets;
+
+    public int score {  get; private set; }
+    public int lives { get; private set;}
+
+    private void Start()
     {
-        
+        NewGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (this.lives <= 0 && Input.anyKeyDown)
+        {
+            NewGame();
+        }
     }
+
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+        NewRound();
+    }
+
+    private void NewRound()
+    {
+        foreach(Transform pellet in this.pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+        ResetState();
+    }
+    private void ResetState()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(true);
+        }
+        this.pacman.gameObject.SetActive(true);
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(false);
+        }
+        this.pacman.gameObject.SetActive(false);
+    }
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    public void GhostEaten(Ghosts ghost)
+    {
+        SetScore(this.score + ghost.points);
+    }
+    public void PacManEaten(PacMan Pacman)
+    {
+        this.pacman.gameObject.SetActive(false);
+
+        SetLives(this.lives - 1);
+
+        if (this.lives > 0)
+        {
+            Invoke(nameof(ResetState), 3.0f);
+        }
+        else
+        {
+            GameOver();
+        }
+
+    }
+
+
+
 }
+
---
+++ Assets/Scripts/Ghosts.cs
@@ -0,0 +1,7 @@
+
+using UnityEngine;
+
+public class Ghosts : MonoBehaviour
+{
+    public int points = 200;
+}
